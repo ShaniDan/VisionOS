bb52a612797c0acd939ff083ab1378054e3e70a0
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-06-01 14:43:14 -0700

    volume

--- Spline-VisionOS/Spline-VisionOS/ContentView.swift
+++ Spline-VisionOS/Spline-VisionOS/ContentView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 import SplineRuntime
 
 struct ContentView: View {
+    
+    @State private var isShowingVolume = false
+    @State private var isShowingImmersiveSpace = false
+    
+    @Environment(\.openWindow) var openWindow
+    @Environment(\.dismissWindow) var dismissWindow
+    
     var body: some View {
         VStack {
            CharacterView()
+            
+            HStack {
+                Toggle("Show Volume", isOn: $isShowingVolume)
+                    .toggleStyle(.button)
+                    .onChange(of: isShowingVolume) { _, isShowing in
+                        if isShowing {
+                            openWindow(id: "volume")
+                        } else {
+                            dismissWindow(id: "volume")
+                        }
+                    }
+            }.padding()
         }
         .padding()
     }
@@ -30,5 +49,14 @@ struct CharacterView: View {
         let url = URL(string: "https://build.spline.design/NMp83bor-xtTS3yh1HjB/scene.splineswift")!
 
         SplineView(sceneFileURL: url).ignoresSafeArea(.all)
+    }
+}
+
+struct VolumeView: View {
+    var body: some View {
+        // fetching from cloud
+        let url = URL(string: "https://build.spline.design/wVQTGW-DB7bw-ANq6D3v/scene.splineswift")!
+
+        SplineVolumetricContent(sceneFileURL: url)
     }
 }
--- Spline-VisionOS/Spline_VisionOSApp.swift
+++ Spline-VisionOS/Spline_VisionOSApp.swift
@@ -13,5 +13,11 @@ struct Spline_VisionOSApp: App {
         WindowGroup {
             ContentView()
         }
+        
+        WindowGroup(id: "volume") {
+            VolumeView()
+        }
+        .windowStyle(.volumetric)
+        .defaultSize(width: 0.3, height: 0.3, depth: 0.3, in: .meters)
     }
 }
